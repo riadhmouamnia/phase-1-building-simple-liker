d82e299f36b826c4eacb80a9f070ba63516214d2
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-22 18:18:25 +0100

    Completed assignment

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
@@ -23,3 +20,23 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+const hearts = document.querySelectorAll(".like-glyph");
+mimicServerCall()
+  .then((data) => {
+    console.log(data);
+    hearts.forEach((heart) => {
+      heart.addEventListener("click", () => {
+        heart.innerText === FULL_HEART
+          ? (heart.innerText = EMPTY_HEART)
+          : (heart.innerText = FULL_HEART);
+        heart.classList.toggle("activated-heart");
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    const modal = document.querySelector("#modal");
+    modal.classList.remove("hidden");
+    setTimeout(() => modal.classList.add("hidden"), 3000);
+  });
